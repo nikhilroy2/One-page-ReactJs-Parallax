7cd237a84407723f52ef805db07e3d30bd8a10a9
Your Name <nikhilchandraroypoet@gmail.com>
2020-03-24 06:14:02 +0600

    Updates

---
+++ precache-manifest.200351c01e0fe6fc48d0adcbf52ec25b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201739712a57f3ad4aaa4b5103616608",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec525ef235bc20ef699e",
+    "url": "./static/css/2.b1afbf4b.chunk.css"
+  },
+  {
+    "revision": "cb375a8b28fd09fa76a0",
+    "url": "./static/css/main.ba9a4e4d.chunk.css"
+  },
+  {
+    "revision": "ec525ef235bc20ef699e",
+    "url": "./static/js/2.a5506b4c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.a5506b4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb375a8b28fd09fa76a0",
+    "url": "./static/js/main.f3697c85.chunk.js"
+  },
+  {
+    "revision": "1d1feb9ac959fa64e717",
+    "url": "./static/js/runtime-main.33d7aa7f.js"
+  },
+  {
+    "revision": "6023b7fdcf85eab2ea94fc2d113db5bd",
+    "url": "./static/media/cursoo.6023b7fd.cur"
+  },
+  {
+    "revision": "f67e2d1850ca22f2d8b794a75c7c7ed6",
+    "url": "./static/media/music.f67e2d18.mp3"
+  }
+]);
